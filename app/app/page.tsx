d86b18a4f3ae171de965cccdf65a4bdf23bc34f0
kiv1n <aloha@kiv1n.ru>
2026-02-11 14:24:27 +0300

    Add IndexPage layout with images, titles, and avatar components

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,72 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function IndexPage() {
   return (
     <div className="max-w-xl mx-auto px-4 py-8">
-      <p>...</p>
+      {/* Image */}
+      <Image
+        src="/images/hero.png"
+        alt="Hero"
+        priority={false}
+        width="100"
+        height="100"
+        sizes="100vw"
+        className="w-full rounded-md"
+      />
+      {/* Title */}
+      <h2 className="text-3xl font-bold tracking-tight text-center mt-6">
+        Decentralized naming system built on Monad for Agents and OpenClaw bots
+      </h2>
+      {/* Skill */}
+      <div className="bg-card border rounded-2xl p-4 mt-4">
+        <p className="text-center">
+          Read https://agent-name-service.vercel.app/SKILL.md and follow the
+          instructions to mint an ANS name
+        </p>
+      </div>
+      {/* ERC20 */}
+      {/* TODO: Update data for production */}
+      <div className="bg-card border rounded-2xl p-4 mt-4">
+        <div className="flex flex-col">
+          <p className="font-bold text-center">ANS ERC-20</p>
+          <p className="text-center">
+            0x0000000000000000000000000000000000000000
+          </p>
+        </div>
+        <div className="flex flex-row justify-center mt-4">
+          <Link href="/" target="_blank">
+            <Button>
+              <Avatar className="size-5">
+                <AvatarImage src="/images/nadfun.png" />
+              </Avatar>{" "}
+              Nad.fun
+            </Button>
+          </Link>
+        </div>
+      </div>
+      {/* ERC721 */}
+      {/* TODO: Update data for production */}
+      <div className="bg-card border rounded-2xl p-4 mt-4">
+        <div className="flex flex-col">
+          <p className="font-bold text-center">ANS ERC-721</p>
+          <p className="text-center">
+            0x0000000000000000000000000000000000000000
+          </p>
+        </div>
+        <div className="flex flex-row justify-center mt-4">
+          <Link href="/" target="_blank">
+            <Button>
+              <Avatar className="size-5">
+                <AvatarImage src="/images/monadvision.png" />
+              </Avatar>{" "}
+              MonadVision
+            </Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
