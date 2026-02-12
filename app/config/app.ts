c4a9ee69f91c393ba75a767b6df18ca9da52bd46
kiv1n <aloha@kiv1n.ru>
2026-02-12 09:33:35 +0300

    Update erc721Address in appConfig to the correct production address

--- app/config/app.ts
+++ app/config/app.ts
@@ -8,5 +8,5 @@ export const appConfig = {
   },
   github: "https://github.com/web3goals/agent-name-service-prototype",
   erc20Address: "0xB31e1c705eE3A1D4D458C61694DD75512e337777",
-  erc721Address: "0x0000000000000000000000000000000000000000", // TODO: Update address for production
+  erc721Address: "0xed3c8bc34b29d1fd9b5a7e8935730d98920187a1",
 };
